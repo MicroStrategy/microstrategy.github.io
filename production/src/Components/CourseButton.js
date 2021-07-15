20194154eb8a630b5d280a23b69798b23c91ccca
Amber Wang <amwang@microstrategy.com>
2021-07-15 14:57:38 -0400

    enlarge course name

--- production/src/Components/CourseButton.js
+++ production/src/Components/CourseButton.js
@@ -5,7 +5,7 @@ function CourseButton(props) {
     return (
         <button className="course-button" onClick={function() {window.location = props.link;}}>
         <img src={props.src} alt={props.name} id="course-img"/>
-        <h4 class="course-name">{props.name}</h4>
+        <h2 class="course-name">{props.name}</h2>
         <p class="course-desc">{props.desc}</p>
 
         </button>
