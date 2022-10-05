1d69146d35dceebca8a24ab185c77304b363fa3e
Du, Xiaochen (Harry) <xdu@microstrategy.com>
2022-10-04 21:10:55 -0400

    Updates

---
+++ precache-manifest.257453b1768718d04d3a60cfe29950cf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c51a7a9ba6d71dd6a7bd",
+    "url": "./static/css/main.808a373b.chunk.css"
+  },
+  {
+    "revision": "c51a7a9ba6d71dd6a7bd",
+    "url": "./static/js/main.c51a7a9b.chunk.js"
+  },
+  {
+    "revision": "7a54d82d9f31ec9e5419",
+    "url": "./static/js/1.7a54d82d.chunk.js"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "9f7d80eae6d7bd1ed4c533e9c851ba06",
+    "url": "./static/media/banner.9f7d80ea.jpg"
+  },
+  {
+    "revision": "556d2ef5bde219fa4ef8d953bf303b04",
+    "url": "./index.html"
+  }
+];
